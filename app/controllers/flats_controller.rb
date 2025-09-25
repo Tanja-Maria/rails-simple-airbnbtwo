af233035081e91c0b19198bb901d9f20fbf40c20
Tanja Heinz <heinz.tanjamaria@gmail.com>
2025-09-25 16:43:42 +0200

    add a new flat

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -7,7 +7,19 @@ class FlatsController < ApplicationController
     @flat = Flat.new
   end
 
+  def create
+    flat = Flat.new(flat_params)
+    flat.save
+    redirect_to flats_path(flat)
+  end
+
   def show
     @flat = Flat.find(params[:id])
+  end
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
   end
 end
